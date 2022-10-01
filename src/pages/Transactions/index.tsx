d324c6613815260e99a997333e289bada8ac01d6
Felipe Macci <felipemacci@gmail.com>
2022-10-01 11:29:45 -0300

    creating transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,35 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
     <>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>2022/04/13</td>
+            </tr>
+            <tr>
+              <td width="50%">Hambúrguer</td>
+              <td>
+                <PriceHighlight variant="expense">- $ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>2022/04/18</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </>
   )
 }
